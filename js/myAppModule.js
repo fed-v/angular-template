c43b88120ba04f318703597bd75db930e96c4b8c
fED <fed_v@hotmail.com>
2015-06-08 12:36:15 -0400

    Added some directives
    Added repeat, show and hide directives and a scope function to my
    controller.

--- js/myAppModule.js
+++ js/myAppModule.js
@@ -2,7 +2,7 @@
 /***            APP MODULE               ***/
 /*******************************************/
 
-var myAppModule = angular.module('myAppModule', []);
+var app = angular.module('myAppModule', []);
 
 
 
@@ -10,18 +10,24 @@ var myAppModule = angular.module('myAppModule', []);
 /***            CONTROLLERS              ***/
 /*******************************************/
 
-myAppModule.controller('myFilterDemoCtrl', function($scope){
+app.controller('myFilterDemoCtrl', function($scope){
 		
 		// Data goes here in Array or Object form!
-		$scope.myData = {
+		$scope.data = {
 			
 			firstname: 'JANET',
 			lastname: 'michaels',
 			dateJoined: new Date(2010, 2, 23),
 			consumption: 123.5698745983,
-			plan: 'super-duper-plan'
+			plan: 'super-duper-plan',
+            myArray: ["str1", "str2", "str3", "str4", "str5"]
 			
 		};
+        
+        // Behaviour functions 
+        $scope.welcome = function (){
+            alert("Welcome!");
+        }
 		
 	}
 );
@@ -32,13 +38,13 @@ myAppModule.controller('myFilterDemoCtrl', function($scope){
 /***           CUSTOM FILTERS            ***/
 /*******************************************/
 
-myAppModule.filter('stripDashes', function(){
+app.filter('stripDashes', function(){
 	return function(str){
 		return str.split('-').join(' ');
 	};
 });
 
-myAppModule.filter('toTitleCase', function(){
+app.filter('toTitleCase', function(){
 	return function(str){
 		return str.replace(/\w\S*/g, function(txt){ return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 	};
